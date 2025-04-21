73f1d4682be461c9cd100a685dd7cc654a322ed2
Krishnac50 <krishnachaithanyan4@gmail.com>
2025-04-21 14:01:44 +0000

    com

---
+++ Project/src/test/java/runner/TestMadeInChina.java
@@ -0,0 +1,30 @@
+package runner;
+
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+
+import utils.Base;
+import utils.Reporter;
+
+public class TestMadeInChina extends Base{
+    public static ExtentReports reports;
+    public static ExtentTest test;
+
+    @BeforeClass
+    public void report(){
+        reports = Reporter.generateExtentReport("Hot Items");
+    }
+    @BeforeMethod
+    public void open(){
+        openBrowser();
+    }
+    @Test
+    public void hotItems(){
+
+    }
+    
+}
