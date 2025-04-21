beb31477d4f9f8be2fae7704eb863dbdf313d6bc
Krishnac50 <krishnachaithanyan4@gmail.com>
2025-04-21 13:24:25 +0000

    krishna code

---
+++ Project/src/test/java/uistore/MadeInChinaPageLocator.java
@@ -0,0 +1,18 @@
+package uistore;
+
+import org.openqa.selenium.By;
+
+public class MadeInChinaPageLocator {
+    public static By furniture = By.cssSelector("a[href = 'https://www.made-in-china.com/quick-products/Furniture.html?pv_id=1ipboqgrf0d0&faw_id=1ipbq21osec&bv_id=1ipbq21ouc3b']");
+    public static By apparelAndClothing = By.cssSelector("a[href = '/quick-products/Apparel-Clothing.html']");
+    public static By bagsCasesAndLuggages = By.cssSelector("a[href = '/quick-products/Bags-Cases-Luggages.html']");
+    public static By computerProducts = By.cssSelector("a[href = '/quick-products/Computer-Products.html']");
+    public static By consumerElectronics = By.cssSelector("a[href = '/quick-products/Consumer-Electronics.html']");
+    public static By electricalEquipment = By.cssSelector("a[href = '/quick-products/Electrical-Equipments.html']");
+    public static By electricalCompents = By.cssSelector("a[href = '/quick-products/Electronic-Components.html']");
+    public static By giftsCrafts = By.cssSelector("a[href = '/quick-products/Gifts-Crafts-Collectibles.html']");
+    public static By motorCycles = By.cssSelector("a[href = '/quick-products/Motorcycles-Scooters.html']");
+    public static By sealingPackaging = By.cssSelector("a[href = '/quick-products/Sealing-Packaging-Storage-Shelving.html']");
+    public static By buildingMaterials = By.cssSelector("a[href = '/quick-products/Building-Materials-Supplies.html']");
+}
+
