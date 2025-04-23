f280f5d96066d5a09165e3eb1c01d4c225452014
pruthvi-22 <battipruthviraj@gmail.com>
2025-04-23 04:16:28 +0000

    almighty push

--- Project/src/test/java/runner/TestMadeInChina.java
+++ Project/src/test/java/runner/TestMadeInChina.java
@@ -142,7 +142,7 @@ public class TestMadeInChina extends Base{
     * d.Return Type: void
     * e.Parameter List: None
     */
-    @Test(priority = 9)
+    @Test(priority = 9,enabled = true)
     public void navigation(){
         test=report.createTest("Navigation Between Categories");
         MadeInChinaNavigationPage madeInChinaNavigationPage=new MadeInChinaNavigationPage(test);
@@ -155,7 +155,7 @@ public class TestMadeInChina extends Base{
     * d.Return Type: void
     * e.Parameter List: None
     */
-	@Test(priority = 10)
+	@Test(priority = 10,enabled = true)
 	public void productCategory(){
 		test=report.createTest("Product Category");
 		MadeInChinaProductDirectoryPage madeInChinaProductDirectoryPage=new MadeInChinaProductDirectoryPage(test);
