4f3ed2098eea5a140eb5fb83cc1f64edc26b1196
Krishnac50 <krishnachaithanyan4@gmail.com>
2025-04-24 12:30:12 +0000

    commited

--- Project/src/test/java/uistore/MadeInChinaWiresAndFurnitureLocatores.java
+++ Project/src/test/java/uistore/MadeInChinaWiresAndFurnitureLocatores.java
@@ -3,11 +3,12 @@ package uistore;
 import org.openqa.selenium.By;
 
 public class MadeInChinaWiresAndFurnitureLocatores {
-    public static By searchBar = By.cssSelector("input[class = 'nail-search-input']");
-    public static By searIcon = By.id("J-nail-search-submit");
-    public static By wireResult = By.cssSelector("em[class = 'product_word']");
-    public static By firstProduct =By.xpath("(//div[@class = 'prod-content']//h2[@class = 'product-name']//a)[1]");
-    public static By contactSupplier = By.cssSelector("div[class = 'button-block contact-btn'] a[class = 'btns button-link-contact']");
-    public static By clickOn2InPagination = By.xpath("//a[text() = '2']");
-    public static By logo = By.cssSelector("a[href='//www.made-in-china.com']");
+    private MadeInChinaWiresAndFurnitureLocatores(){}
+    public static final By searchBar = By.cssSelector("input[class = 'nail-search-input']");
+    public static final By searIcon = By.id("J-nail-search-submit");
+    public static final By wireResult = By.cssSelector("em[class = 'product_word']");
+    public static final By firstProduct =By.xpath("(//div[@class = 'prod-content']//h2[@class = 'product-name']//a)[1]");
+    public static final By contactSupplier = By.cssSelector("div[class = 'button-block contact-btn'] a[class = 'btns button-link-contact']");
+    public static final By clickOn2InPagination = By.xpath("//a[text() = '2']");
+    public static final By logo = By.cssSelector("a[href='//www.made-in-china.com']");
 }
