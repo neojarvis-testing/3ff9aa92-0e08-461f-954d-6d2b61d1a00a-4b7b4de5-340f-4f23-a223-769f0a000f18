612c620f192fa9cb233eb42e2d22a58c7116a8f7
Krishnac50 <krishnachaithanyan4@gmail.com>
2025-04-24 10:51:33 +0000

    updated sonar

--- Project/src/test/java/utils/Reporter.java
+++ Project/src/test/java/utils/Reporter.java
@@ -3,21 +3,32 @@ package utils;
 import java.io.File;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
-import java.util.Base64;
 import java.util.Date;
+import java.util.Properties;
 import com.google.common.io.Files;
+import jakarta.mail.PasswordAuthentication;
+import jakarta.mail.Session;
+import jakarta.mail.internet.InternetAddress;
+import jakarta.mail.Transport;
+import jakarta.mail.internet.MimeBodyPart;
+import jakarta.mail.internet.MimeMessage;
+import jakarta.mail.internet.MimeMultipart;
 import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.ExtentTest;
 import com.aventstack.extentreports.MediaEntityBuilder;
 import com.aventstack.extentreports.Status;
 import com.aventstack.extentreports.reporter.ExtentSparkReporter;
-import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
- 
+import jakarta.mail.BodyPart;
+import jakarta.mail.Message;
+import jakarta.mail.MessagingException;
+import jakarta.activation.DataHandler;
+import jakarta.activation.DataSource;
+import jakarta.activation.FileDataSource;
+import jakarta.mail.Multipart;
 public class Reporter extends Base {
     public static TakesScreenshot ts;
-    
     /*
      * a.Method Name: generateExtentReport
      * b.Author Name: Shashank Kondur
@@ -27,53 +38,10 @@ public class Reporter extends Base {
      */
     public static ExtentReports generateExtentReport(String reportName) {
         ExtentReports extentReport = new ExtentReports();
-        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy.MM.dd.HH.mm.ss");
-        String timestamp = dateFormat.format(new Date());
-        String reportFilePath = System.getProperty("user.dir") + "/reports/";
-        reportFilePath += reportName + "_" + timestamp + ".html";
-        File extentReportFile = new File(reportFilePath);
+        File extentReportFile = new File(reportName);
         ExtentSparkReporter sparkReporter = new ExtentSparkReporter(extentReportFile);
         extentReport.attachReporter(sparkReporter);
         return extentReport;
-    }
-
-    /*
-     * a.Method Name: takeScreenshotAsBase64
-     * b.Author Name: Shashank Kondur
-     * c.Description: Takes the Screenshot as Base64 format.
-     * d.Return Type: String
-     * e.Paramenter List: String reportName
-     */
-    public static String takeScreenshotAsBase64() {
-        String base64String = "";
-        try {
-            File screenshotFile = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
-            byte[] fileContent = FileUtils.readFileToByteArray(screenshotFile);
-            base64String = Base64.getEncoder().encodeToString(fileContent);
-        } catch (Exception e) {
-            LoggerHandler.error("takeScreenshotAsBase64 not working");
-        }
-        return base64String;
-    }
-
-    /*
-     * a.Method Name: takeScreenshotAsBase64
-     * b.Author Name: Shashank Kondur
-     * c.Description: Attach the Screenshot to report in base64 format.
-     * d.Return Type: void
-     * e.Paramenter List: String reportName, ExtentTest test, String description
-     */
-    public static void attachScreenshotToReportBase64(String reportName, ExtentTest test, String description) {
-        try {
-            String base64Screenshot = takeScreenshotAsBase64();
-            if (!base64Screenshot.isEmpty()) {
-                test.info(description, MediaEntityBuilder.createScreenCaptureFromBase64String(base64Screenshot).build());
-            } else {
-                test.log(Status.FAIL, "Screenshot capture failed.");
-            }
-        } catch (Exception e) {
-            LoggerHandler.error("attachScreenshotToReportBase64 not working");
-        }
     }
 
     /*
@@ -83,14 +51,14 @@ public class Reporter extends Base {
      * d.Return Type: String
      * e.Paramenter List: String filename
      */
-    public static String captureScreenShot(String filename) {
+    public static String captureScreenshot(String filename) {
         String timestamp = new SimpleDateFormat("yyyy.MM.dd.HH.mm.ss").format(new Date());
         String name = filename + timestamp + ".png";
-        String destPath =  "./"+name;
+        String destPath = "./" + name;
         ts = (TakesScreenshot) driver;
         File file = ts.getScreenshotAs(OutputType.FILE);
         File screenshotsDir = new File(System.getProperty("user.dir") + "/reports");
-       
+ 
         if (!screenshotsDir.exists()) {
             screenshotsDir.mkdirs();
         }
@@ -98,11 +66,10 @@ public class Reporter extends Base {
         try {
             Files.copy(file, target);
         } catch (IOException e) {
-            LoggerHandler.error("captureScreenShot not working");
+            e.printStackTrace();
         }
         return destPath;
     }
-
 
     /*
      * a.Method Name: attachScreenshotToReport
@@ -113,13 +80,62 @@ public class Reporter extends Base {
      */
     public static void attachScreenshotToReport(String filename, ExtentTest test, String description) {
         try {
-            test.log(Status.INFO, description, MediaEntityBuilder.createScreenCaptureFromPath(captureScreenShot(filename)).build());
+            test.log(Status.INFO, description, MediaEntityBuilder.createScreenCaptureFromPath(captureScreenshot(filename)).build());
         } catch (Exception e) {
-            LoggerHandler.error("attachScreenshotToReport not working");
+            e.printStackTrace();
         }
-   
     }
-    
 
+    /*
+    * Method Name   : sendEmailWithReport
+    * Author Name   : Shashank Kondur
+    * Description   : Sends an email with the attached test report.
+    * Return Type   : void
+    * Parameter List: reportFilePath (String): Path to the report file to be attached.
+    */
+    public static void sendEmailWithReport(String reportFilePath) {
+        Properties props = new Properties();
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.smtp.host", "smtp.gmail.com");
+        props.put("mail.smtp.port", "587");
+ 
+        Session session = Session.getInstance(props, new jakarta.mail.Authenticator() {
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication("rajaram225773@gmail.com", "lizfdfgnpvwberzt");
+            }
+        });
+ 
+        try {
+            Message message = new MimeMessage(session);
+            message.setFrom(new InternetAddress("rajaram225773@gmail.com"));
+            message.setRecipients(Message.RecipientType.TO, InternetAddress.parse("sooryavanshibhai@gmail.com"));
+            message.setSubject("Automated Test Report");
+ 
+            BodyPart messageBodyPart = new MimeBodyPart();
+            messageBodyPart.setText("Please find the attached test report.");
+ 
+            Multipart multipart = new MimeMultipart();
+            multipart.addBodyPart(messageBodyPart);
+ 
+            // Attach the report
+            messageBodyPart = new MimeBodyPart();
+            DataSource source = new FileDataSource(reportFilePath);
+            messageBodyPart.setDataHandler(new DataHandler(source));
+            messageBodyPart.setFileName(reportFilePath);
+            multipart.addBodyPart(messageBodyPart);
+ 
+            // Combine message parts
+            message.setContent(multipart);
+ 
+            // Send the email
+            Transport.send(message);
+ 
+            System.out.println("Email sent successfully with the report attached.");
+ 
+        } catch (MessagingException e) {
+            e.printStackTrace();
+        }
+    }
 }
  
